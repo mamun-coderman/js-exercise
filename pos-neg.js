5c3c309a5c0ab4cf0f6e4b652f313bba44f9257d
mamun <mamun.coderman@gmail.com>
2024-08-17 00:48:34 +0600

    positive-negetive programme added

---
+++ pos-neg.js
@@ -0,0 +1,11 @@
+// code to check a number positive or negetive
+
+var number = 10;
+
+if (number > 0){
+    console.log("Positive number");
+}else if(number < 0){
+    console.log("Negetive number");
+}else{
+    console.log("Number is Zero");
+}
---
+++ practice.js
@@ -0,0 +1,8 @@
+// program for finding a factorial of a number
+var last_number = 100;
+var factorial = 1;
+
+for (var i = 1; i <= last_number; i++){
+    factorial *= i;
+    console.log("Factorial of " + i + " is " + factorial)
+}

aee4df7cde3e0698a960ff8779a0bdf8ca10d37f
mamun <mamun.coderman@gmail.com>
2024-08-17 01:05:30 +0600

    voting eligibility program added

---
+++ voting-eligibility.js
@@ -0,0 +1,9 @@
+// This programme is checking the eligibility of a user for voting. If user's age is greater than 18, he/she is eligible. Otherwide not.
+
+var age = 32;
+
+if (age > 18){
+    console.log("You are eligible to vote.")
+}else{
+    console.log("You are not eligible to vote.")
+}

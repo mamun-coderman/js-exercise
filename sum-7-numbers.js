c6c2453ae801641bc13f9782834306d051385d31
mamun <mamun.coderman@gmail.com>
2024-09-01 03:32:45 +0600

    1 to 10 sum

---
+++ sum-7-numbers.js
@@ -0,0 +1,9 @@
+// Program to calculate the numbers from 1 to 10
+
+let sum = 0;
+
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+    sum += i;
+}
+console.log("The sum of these numbers are: " + sum);
